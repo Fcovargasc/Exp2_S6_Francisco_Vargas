da2cea2e631a816ad05fe2a3a86e619f1cf91fb1
Fcovargasc <fr.vargasc@duocuc.cl>
2025-04-18 15:58:18 -0400

    Add files via upload

--- Exp2_S6_Francisco_Vargas.java
+++ Exp2_S6_Francisco_Vargas.java
@@ -3,7 +3,7 @@ package exp_s6_francisco_vargas;
 
 import java.util.Scanner;
 
-public class Exp2_S6_Francisco_Vargas {
+public class NumerosPares {
     static int acomulador=0;
     static String acomuladorAsiento="";
     static String numeroAsiento="";
@@ -11,6 +11,7 @@ public class Exp2_S6_Francisco_Vargas {
     
     public static void main(String[] args) {
        Scanner scanner=new Scanner(System.in);
+      
         String remplazo=" ";
         String asientos= "1 2 3\n4 5 6 \n7 8 9 ";
         String asientoModificar;             
@@ -24,9 +25,7 @@ public class Exp2_S6_Francisco_Vargas {
         System.out.println("Bienvenidos al Cafe Moro");
         System.out.println("1 para Reserva de entradas 2 para Compra de entradas 3 Para modificar una venta existente 4 imprimir boletas");
         opcion=scanner.nextInt();
-        if (opcion3==2 && opcion==2){
-                    acomuladorAsiento=" ";
-                    acomulador=0;}
+        
         }while(opcion>4||opcion<1);
         
         switch (opcion){
@@ -44,8 +43,8 @@ public class Exp2_S6_Francisco_Vargas {
                       if (acomuladorAsiento.contains(numeroAsiento)){
                     System.out.println("asiento no disponible");
                     i--;
-                    acomuladorAsiento=acomuladorAsiento.replace(numeroAsiento, remplazo);
-                          System.out.println("OWO "+acomuladorAsiento);
+                    acomuladorAsiento=acomuladorAsiento.replace(numeroAsiento, remplazo);//BReakpoint 4
+                          
                     acomulador-=valorEntrada;
                     cantidadEntrada--;
              }
@@ -60,22 +59,57 @@ public class Exp2_S6_Francisco_Vargas {
                 System.out.println("para salir precione opcion 1 si sale sin imprimir la boleta la reserva se cancelara");
              }
             case 2 ->{
-                System.out.println("¿Cuantas entradas quiere comprar?");
+                
+                if(cantidadEntrada>=1){
+                    System.out.println("Ssi continua se borrara su reserva si quiere continuar con la reserva imprima su boleta  ");
+                    do{
+                    System.out.println("1 para imprimir 2 para continuar");
+                    opcion2=scanner.nextInt();}while(opcion2<1||opcion2>2);
+                     if (opcion2==1){
+                    System.out.println("ubicacion de Asientos: "+ acomuladorAsiento);
+                    System.out.println("cantidad de entradas: "+ cantidadEntrada);
+                    System.out.println("valor total: " + acomulador);
+                   
+                    acomuladorAsiento=" ";
+                    acomulador=0;
+                    break;
+                }else if(opcion2==2){
+                    cantidadEntrada-=cantidadEntrada;
+                    acomuladorAsiento=" ";
+                    acomulador=0;
+                }
+                
+                }
+                
+                 System.out.println("¿Cuantas entradas quiere reservar?");
                 numeroEntradas=scanner.nextInt();
                 System.out.println(asientos);
-                                
                 for (int i = 1; i <= numeroEntradas; i++) {
                     System.out.println("elija numero de asiento");
-                numeroAsiento=scanner.next();
-                acomuladorAsiento+=numeroAsiento+" ";
-                cantidadEntrada++;
-                acomulador+=valorEntrada;}//breakpoint 2
+                    numeroAsiento=scanner.next();
+                      if (acomuladorAsiento.contains(numeroAsiento)){
+                    System.out.println("asiento no disponible");
+                    i--;
+                    acomuladorAsiento=acomuladorAsiento.replace(numeroAsiento, remplazo);//BReakpoint 3
+                          
+                    acomulador-=valorEntrada;
+                    cantidadEntrada--;
+             }
+                    acomuladorAsiento+=numeroAsiento+" ";
+                    cantidadEntrada++;
+                    acomulador+=valorEntrada;//breakpoint 1
+                    
+              }
                 System.out.println("¿Quiere Imprimir Boleta?presione 1 para si presione 2 para no");
                 opcion=scanner.nextInt();
                 if (opcion==1){
                     System.out.println("ubicacion de Asientos: "+ acomuladorAsiento);
-                    System.out.println("cantidad de entradas: "+ numeroEntradas);
+                    System.out.println("cantidad de entradas: "+ cantidadEntrada);
                     System.out.println("valor total: " + acomulador);
+                    
+                    acomuladorAsiento=" ";
+                    acomulador=0;
+                    numeroEntradas=0;
                     
                 }
                 
@@ -114,6 +148,7 @@ public class Exp2_S6_Francisco_Vargas {
                     System.out.println("ubicacion de Asientos: "+ acomuladorAsiento);
                     System.out.println("cantidad de entradas: "+ cantidadEntrada);
                     System.out.println("valor total: " + acomulador);
+                    
                     acomuladorAsiento=" ";
                     acomulador=0;
                 }
@@ -129,8 +164,9 @@ public class Exp2_S6_Francisco_Vargas {
         
         }while(opcion3<1 || opcion3>2);
         }while (opcion3 !=1);
-        
+        scanner.close();
         System.out.println("gracias por venir a Cafe Moro"); 
     }
   
 }
+
